3ff198c20377459e09c95d69bfdf1cea95a95c67
Ratnadeep <ratnadeeppersonal@gmail.com>
2018-07-05 16:27:42 +0530

    updated functionality

--- js/designs.js
+++ js/designs.js
@@ -1,43 +1,118 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-
-
-// the following code is just to make the table without other function enable.
-
-$('.makeGrid').click(function () {
+let hasTable = false;
+$('#submitBtn').click(function () {
+  checkStatus();
+});
+// alert fucntion for existing table
+function checkStatus() {
+  if (hasTable == true) {
+    if (confirm("Are your sure? This will Reset all progress you made.")) {
+      makeGrid();
+    }
+  } else {
+    makeGrid();
+  }
+}
+//  table creating function
+function makeGrid() {
+  hasTable = true;
+  $('.paint__tools').css("display", "flex");
+  $('.tools__head').css("display", "block");
   removeTable(); // reset the Grid
   let canvasHeight = $('#inputHeight').val(); // assign input height to canvasHeight variable.
-  let canvasWidth = $('#inputWeight').val(); // asssign input width to canvasWidth variable.
-  // Your code goes here!
-  for (i = 0; i < canvasHeight; i++) {
-    $('#pixelCanvas').append('<tr></tr>'); // add table row to the table.
-    for (j = 0; j < canvasWidth; j++) {
-      $('#pixelCanvas').append('<td></td>'); // add table data cell to the table.
+  let canvasWidth = $('#inputWidth').val(); // asssign input width to canvasWidth variable.
+  if ((canvasHeight != "") && (canvasWidth != "")) {
+    for (i = 0; i < canvasHeight; i++) {
+      $('#canvas').append('<tr></tr>'); // add table row to the table.
+      for (j = 0; j < canvasWidth; j++) {
+        $('tr:last').append('<td></td>'); // add table data cell to the table.
 
+      }
     }
+  } else {
+    alert("Please Choose Grid Size by providing the correct number!!!");
   }
-});
-
+}
 
 
 function removeTable() { // function for removing existing table.
-  $('#pixelCanvas').empty(); // removes the table. 
+  $('#canvas').empty(); // removes the table. 
 }
 
-// Color picker for painting
-$('table').on('click', "td", function () { // color picker function.
-  let paintColor = $('#colorPicker').val(); // assigns inputted color to the paintColor variable.
-  $(this).css("background-color", paintColor);
+// tool select effect function 
+let tool;
+let mouseState = false;
+// brush select effect
+$('.paint__brush').on("click", function () {
+  tool = "brush";
+  $('.fa-paint-brush').css("border", "1px solid rgb(170, 170, 170)");
+  $('.fa-paint-brush').css("box-shadow", "5px 5px 10px grey");
+  $('.fa-eraser').css("border", "none");
+  $('.fa-eraser').css("box-shadow", "none");
+});
+// eraser select effect
+$('.eraser').on("click", function () {
+  tool = "eraser";
+  $('.fa-eraser').css("border", "1px solid rgb(170, 170, 170)");
+  $('.fa-eraser').css("box-shadow", "5px 5px 10px grey");
+  $('.fa-paint-brush').css("border", "none");
+  $('.fa-paint-brush').css("box-shadow", "none");
 });
 
 
-$('table').on('dblclick', "td", function () { // erase function which sets the background color to no-color by double clicking on it.
-  $(this).css("background-color", "");
+// these two event listener is setting the mouse state
+$(document).on("mousedown", function () {
+  mouseState = true;
+})
+$(document).on("mouseup", function () {
+  mouseState = false;
+})
+
+//  paint on click function 
+
+$('table').on("click", "td", function () {
+  let paintColor = $('#colorPicker').val();
+  if (tool === "brush") {
+    $(this).css("background-color", paintColor);
+  } else if (tool === "eraser") {
+    $(this).css("background-color", "");
+  }
+})
+
+// mouse left click and drag to paint function and eraser fucntion 
+
+$('table').on('mousemove', 'td', function () { // color picker function.
+  let paintColor = $('#colorPicker').val();
+  if ((mouseState == true) && (tool === "brush")) {
+    $(this).css("background-color", paintColor);
+  } else if ((tool === "eraser") && (mouseState === true)) {
+    $(this).css("background-color", "");
+  }
 });
 
-function resetColor() {
-  $('td').css({"background-color": ""});
-}
+//           color reset function 
+// function resetColor() {
+//   $('td').css({
+//     "background-color": ""
+//   });
+// }
+
+//  delete table function 
+
+$('.close').on("click", function () {
+  $('#canvas').empty();
+});
+
+// color reset function
+
+$('.reset').on("click", function () {
+  $('td').css("background-color", "");
+})
+
+// event listener for enter key to trigger the submit button 
+
+$('#inputWidth').on("keydown", function (e) {
+  let key = e.keyCode;
+  if (key === 13) {
+    checkStatus();
+  }
+});
